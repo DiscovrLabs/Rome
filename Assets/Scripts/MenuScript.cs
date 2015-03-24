c1a7005472a8bf4a136a80fba060bd871c33726c
MauricioDiaz <mauriciodiaz.1989@yahoo.com>
2015-03-24 14:37:55 -0800

    first commit

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MenuScript : MonoBehaviour {
+
+	public Image SelectionText;
+	public List<Sprite> ItemList = new List<Sprite>();
+	private int itemSpot = 0;
+
+	void Start(){ }
+
+	void Update()
+	{
+		Debug.Log ("spot: "+itemSpot);
+	}
+
+
+	public void RightSelection()
+	{
+		GameObject.Find("toggle_music").SendMessage("playSound");
+		if(itemSpot < 1){ itemSpot = 3; }
+		itemSpot-=1;
+		SelectionText.sprite = ItemList[itemSpot];
+
+	}
+
+	public void LeftSelection()
+	{
+		GameObject.Find("toggle_music").SendMessage("playSound");
+		if(itemSpot > 1){ itemSpot = -1; }
+		itemSpot+=1;
+		SelectionText.sprite = ItemList [itemSpot];
+	
+	}
+
+	public void StartSelection()
+	{
+		if(itemSpot == 0)
+		{
+			GameObject.Find("correct_audio").SendMessage("playSound");
+			Application.LoadLevel ("a1");
+		}
+		else if(itemSpot == 1)
+		{
+			GameObject.Find("incorrect_audio").SendMessage("playSound");
+			//Application.LoadLevel ("");
+		}
+		else if(itemSpot == 2)
+		{
+			GameObject.Find("incorrect_audio").SendMessage("playSound");
+			//Application.LoadLevel ("");
+		}
+	}
+}
---
+++ Assets/Scripts/map_select.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class map_select : MonoBehaviour {
+
+	public float answer;
+	public string level;
+	float guess=0;
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+
+		ray = Camera.current.ScreenPointToRay (new Vector3 ((Screen.width / 2f), (Screen.height / 2f), 0));
+
+		if (Physics.Raycast (ray, out hit)) {
+
+			
+			Debug.Log (hit.collider.name);
+
+			if (Input.GetMouseButtonDown (0)) 
+			{
+
+				if (hit.collider.name == ("Mission_1")) {
+					guess=1;
+				}
+				
+				if (hit.collider.name == ("Explore_1")) {
+					guess=2;
+				}				
+				
+				if (hit.collider.name == ("Abort_1")) {
+					guess=3;
+				}
+				
+				if (hit.collider.name == ("West_1")) {
+					guess=4;
+				}
+				
+				if (hit.collider.name == ("EastN_1")) {
+					guess=5;
+				}
+				
+				if (hit.collider.name == ("EastS_1")) {
+					guess=6;
+				}
+				
+				if (hit.collider.name == ("WestS_1")) {
+					guess=7;
+				}
+				
+				if (hit.collider.name == ("WestN_1")) {
+					guess=8;
+				}
+				check ();
+			}
+		}
+	
+	}
+
+	void check()
+	{
+		if (guess == answer)
+		{
+			Debug.Log ("CORRECT");
+			GameObject.Find("correct_audio").SendMessage("playSound");
+			Application.LoadLevel(level);
+		}
+	}
+}
